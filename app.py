b44500c438e21e243b385ce184ffd0672aececc1
Ang-Ian <angian2005@gmail.com>
2025-01-14 15:48:03 +0800

    I added a rugs.html file and added an app route.

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def helloworld():
     return render_template("index.html")
 
+@app.route("/buymerugs")
+def buymerugs() :
+    return render_template("rugs.html")
+
 if __name__ == "__main__":
     app.run(port=8000, host='0.0.0.0', debug=True)
