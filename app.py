4599402711bfc30a3d1133ed731042d3bb5de904
King Whey <lowkingwhey@gmail.com>
2025-01-14 15:41:45 +0800

    templates and debug=true

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return "Hello World!!!!!!!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(port=8000, host='0.0.0.0')
+    app.run(port=8000, host='0.0.0.0', debug=True)
