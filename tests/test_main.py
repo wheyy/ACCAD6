029a4c8a8e8b842ab43624766bf62fc13d7b3fdd
sleepydirt <117893331+sleepydirt@users.noreply.github.com>
2025-01-16 15:07:19 +0800

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,15 @@ from app import app
 def test_redirect():
     response = app.test_client().get('/')
     assert response.status_code == 302
+    
+def test_connection():
+    response = app.test_client().get('/upload')
+    assert response.status_code == 200
+
+def test_connection1():
+    response = app.test_client().get('/edit')
+    assert response.status_code == 200
+
+def test_connection2():
+    response = app.test_client().get('/calendar')
+    assert response.status_code == 200
