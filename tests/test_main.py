048a9216db7091577fa8d771768dcc14649616b2
sleepydirt <117893331+sleepydirt@users.noreply.github.com>
2025-01-16 13:34:57 +0800

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,20 +1,5 @@
-# Test for connectivity
-import requests
 from app import app
 
-def test_connection():
-    response = app.test_client().get('/upload')
-    assert response.status_code == 200
-
-def test_connection1():
-    response = app.test_client().get('/edit')
-    assert response.status_code == 200
-
-def test_connection2():
-    response = app.test_client().get('/calendar')
-    assert response.status_code == 200
-
-# since we want to redirect / to /update, check if redirect is working
 def test_redirect():
     response = app.test_client().get('/')
     assert response.status_code == 302
